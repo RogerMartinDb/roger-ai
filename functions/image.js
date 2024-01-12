48a90e1e9f99aa78d80d824e64a198a5f9de9620
Roger Martin <roger.martin.db@gmail.com>
2024-01-12 14:24:50 +0000

    add a cache control header

--- functions/image.js
+++ functions/image.js
@@ -13,13 +13,13 @@ export async function onRequest(context) {
     method: 'POST'
   }
 
-  console.log(params)
-
   const apiCall = await fetch(
     'https://api.cloudflare.com/client/v4/accounts/09084f948abbe98e17fc7e0f9597a761/ai/run/@cf/stabilityai/stable-diffusion-xl-base-1.0',
     params
   )
 
-  console.log('response')
-  return new Response(apiCall.body, apiCall)
+  const response = new Response(apiCall.body, apiCall)
+  response.headers.append('Cache-Control', 'max-age=3600')
+
+  return response
 }
