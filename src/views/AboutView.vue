9aace18154ba50ab62ba8e9b0e551baf70eb5710
Roger Martin <roger.martin.db@gmail.com>
2024-01-15 09:50:46 +0000

    set about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About</h1>
+    <p>This is project to play with some AI options</p>
+    <p>You can find the source code at <a href="https://github.com/RogerMartinDb/roger-ai">github.com/RogerMartinDb/roger-ai</a></p>
   </div>
 </template>
 
@@ -8,8 +10,8 @@
 @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
-    display: flex;
-    align-items: center;
+    position: relative;
+    top: 30%;
   }
 }
 </style>
